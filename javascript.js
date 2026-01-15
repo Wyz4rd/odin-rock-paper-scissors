11f056a773e8e49618f343b295cc8fab193c8b02
Kody <39041396+Wyz4rd@users.noreply.github.com>
2026-01-15 22:32:14 +1100

    Add playRound function for logic of a single round

--- javascript.js
+++ javascript.js
@@ -3,14 +3,14 @@ let computerScore = 0;
 
 function getComputerChoice() {
     let result = "";
-    let computerCoice = Math.ceil(Math.random() * 3);
+    let computerChoice = Math.ceil(Math.random() * 3);
     
-    if (computerCoice === 1) {
-        return result + "Rock";
-    } else if (computerCoice === 2) {
-        return result + "Paper"
+    if (computerChoice === 1) {
+        return result = "rock";
+    } else if (computerChoice === 2) {
+        return result = "paper"
     } else {
-        return result + "Scissors"
+        return result = "scissors"
     }
 }
 
@@ -19,5 +19,27 @@ function getHumanChoice() {
     return humanChoice
 }
 
-console.log(getComputerChoice())
-console.log(getHumanChoice())
+function playRound(humanChoice, computerChoice) {
+    humanChoice = getHumanChoice().toLowerCase()
+    console.log(humanChoice)
+    computerChoice = getComputerChoice()
+    console.log(computerChoice)
+    if(humanChoice === computerChoice) {
+        return console.log(`Its a draw! you both chose ${humanChoice}`)
+    } else if(humanChoice === "rock" && computerChoice === "scissors") {
+        humanScore++
+        return console.log(`You win! ${humanChoice} beats ${computerChoice}`)
+    } else if(humanChoice === "paper" && computerChoice === "rock") {
+        humanScore++
+        return console.log(`You win! ${humanChoice} beats ${computerChoice}`)
+    } else if(humanChoice === "scissors" && computerChoice === "paper") {
+        humanScore++
+        return console.log(`You win! ${humanChoice} beats ${computerChoice}`)
+    } else {
+        computerScore++
+        return console.log(`You lose! ${computerChoice} beats ${humanChoice}`)
+    }
+    
+}
+
+console.log(playRound(), playRound(), humanScore, computerScore)
